f948c9362c174805229872686a8873d0734a5c61
USER <USER@DESKTOP-A3K942C>
2021-12-15 19:18:52 +0900

    Commit by a team member

--- src/TestCode.java
+++ src/TestCode.java
@@ -6,6 +6,13 @@ public class TestCode {
 		System.out.println("This is git demo project");
 		System.out.println("It will just print Hello World");
 		System.out.println("More code is added");
+		
+		int a = 5;
+		int b = 10;
+		if (a<b)
+			System.out.println("a is less than b");
+		else
+			System.out.println("a is bigger than b");
 	}
 
 }
