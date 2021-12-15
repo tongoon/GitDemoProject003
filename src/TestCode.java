4aac0231f89920e49531e9c478c70fdd49e075eb
USER <USER@DESKTOP-A3K942C>
2021-12-15 16:32:41 +0900

    First commit.

---
+++ src/TestCode.java
@@ -0,0 +1,11 @@
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is git demo project");
+		System.out.println("It will just print Hello World");
+		System.out.println("More code is added");
+	}
+
+}
